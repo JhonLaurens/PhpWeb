0359dd76ab904ca13776cc52ee8a022c0244b08b
JhonLaurens <jhonlaurens@gmail.com>
2024-05-02 17:00:33 -0500

    update
    actualizacion

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!-- footer.php que contenga 2024 Hotel paradise , all ring reserved y logo de redes sociales como X , ISTAGRAM Y FACEBOOK MENU INICIO. HABITACIONES, CONTACTO, TEAM (DESARROLLO) Y BLOG-->
+<footer>
+    <p>2024 Hotel Paradise, all rights reserved</p>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="habitaciones.php">Habitaciones</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+            <li><a href="team.php">Team</a></li>
+            <li><a href="blog.php">Blog</a></li>
+        </ul>
+    </nav>
+    <div class="redes"> 
+     <a href="https://www.facebook.com/"><img src="./images/rs_f.png" alt="Facebook"></a>
+        <a href="https://www.instagram.com/"><img src="./images/rs_i.png" alt="Instagram"></a>
+        <a href="https://www.twitter.com/"><img src="./images/rs_x.png" alt="Twitter"></a>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hotel Paradise</title>
+    <link rel="stylesheet" type="text/css" href="./images/logo.png">
+    <link rel="icon" href="./PARADISE.png">
+</head>
+<body>
+    <header>
+        <img src="./images/logo.png" alt="Logo">
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <!-- Agrega aquí más enlaces si los necesitas -->
+            </ul>
+        </nav>
+    </header>
